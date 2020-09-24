724bf071a8faf03eab7af54b258046ca358a26d1
s-sutton3 <63067467+s-sutton3@users.noreply.github.com>
2020-09-24 13:33:07 -0600

    A simple tool to analyze numbers
    A tool to determine if a number fits any of multiple parameters, such as if it is positive, negative, zero, even, odd or a multiple of 10.

---
+++ main.js
@@ -0,0 +1,45 @@
+// Number Analyzer
+
+// HTML Elements
+let numEl = document.getElementById('numInput');
+
+// Add Event Listener
+numEl.addEventListener('change', analyzeNumber);
+
+// Event Function
+function analyzeNumber() {
+    // Get Number from Input Element
+    let numInput = Number(numEl.value);
+
+    // Analyze Number and display results
+    document.getElementById('sign').innerHTML = getSign(numInput);
+    document.getElementById('even-odd').innerHTML = evenOrOdd(numInput);
+    document.getElementById('multiple').innerHTML = multipleOf10(numInput);
+}
+
+
+// Analyze Functions
+
+function getSign(num) {
+if (num > 0 ){
+    return "pos"
+    }else if (num < 0) {
+        return "neg"
+    }else {
+            return "zero"
+        }
+    }
+function evenOrOdd(num) {
+if (num % 2 == 0) {
+    return "even"
+    }else {
+            return "odd"
+    }
+}
+function multipleOf10(num) {
+if (num % 10 == 0) {
+    return "yes"
+    }else {
+        return "no"
+    }
+}
